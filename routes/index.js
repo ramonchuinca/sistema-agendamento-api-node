931715570b4f00239dd6a85015bf5711ab6cec8f
ramon chuinca <ramonchuinca@hotmail.com>
2025-06-21 14:55:18 -0400

    ataualizando

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const usuarioCtrl = require('../controllers/usuarioController')
-
-
+const usuarioCtrl = require('../controllers/usuarioController');
 const agendamentoCtrl = require('../controllers/agendamentoController');
-router.get('/',(req,res)=>{res.json({mensage:'efetuado com sucesso'})})
-router.post('/login', usuarioCtrl)
-router.get('/horarios-disponiveis/:data', agendamentoCtrl.horariosDisponiveis)
-router.post('/agendar', agendamentoCtrl.agendar)
-router.get('/painel', agendamentoCtrl.listarPainel)
-router.get('/vagas-restantes', agendamentoCtrl.vagasRestantes) 
 
-module.exports = router
+router.get('/', (req, res) => { res.json({ mensage: 'efetuado com sucesso' }) });
+router.post('/login', usuarioCtrl.login);
+router.get('/horarios-disponiveis/:data', agendamentoCtrl.horariosDisponiveis);
+router.post('/agendar', agendamentoCtrl.store);
+router.get('/painel', agendamentoCtrl.listarPainel);
+router.get('/vagas-restantes', agendamentoCtrl.vagasRestantes);
 
+module.exports = router;
