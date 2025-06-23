f94baf242925eae70831f7ffe6443ffe4341cfc7
ramon chuinca <ramonchuinca@hotmail.com>
2025-06-22 21:17:54 -0400

    atualizado novamente

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,9 @@ router.get('/', (req, res) => { res.json({ mensage: 'efetuado com sucesso' }) })
 router.post('/login', usuarioCtrl.login);
 router.get('/horarios-disponiveis/:data', agendamentoCtrl.horariosDisponiveis);
 router.post('/agendar', agendamentoCtrl.store);
-router.get('/agendamentos-com-usuarios', controller.listarComUsuarios);
+router.get('/agendamentos-com-usuarios', agendamentoCtrl.listarComUsuarios); // ✅ corrigido aqui
 router.use('/', require('./agendamento'));
-
 router.get('/vagas-restantes', agendamentoCtrl.vagasRestantes);
 
 module.exports = router;
+
