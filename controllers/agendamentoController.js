7ef9a8d01941071bda61bac8dd5ce3581a41471c
ramon chuinca <ramonchuinca@hotmail.com>
2025-06-23 13:54:32 -0400

    atualizado

--- controllers/agendamentoController.js
+++ controllers/agendamentoController.js
@@ -115,5 +115,44 @@ exports.vagasRestantes = async (req, res) => {
 
 
 
+// controllers/agendamentoController.js
+// const Agendamento = require('../models/Agendamento');
+// const Usuario = require('../models/Usuario');
+
+// controllers/agendamentoController.js
+const Agendamento = require('../models/Agendamento');
+const Usuario = require('../models/Usuario');
+
+exports.listarPainel = async (req, res) => {
+  try {
+    // Buscar os agendamentos e preencher dados do usuário
+    const agendamentos = await Agendamento.find()
+      .populate('usuario_id', 'nome telefone') // pega nome e telefone do usuário
+      .sort({ data: 1, hora: 1 }); // ordena por data/hora
+
+    res.json(agendamentos);
+  } catch (error) {
+    console.error('Erro ao listar agendamentos:', error);
+    res.status(500).json({ erro: 'Erro ao listar agendamentos' });
+  }
+};
+
+
+
+
+
+
+
+
+exports.deletarAgendamento = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Agendamento.findByIdAndDelete(id);
+    res.json({ mensagem: 'Agendamento deletado com sucesso' });
+  } catch (error) {
+    console.error('Erro ao deletar agendamento:', error);
+    res.status(500).json({ erro: 'Erro ao deletar agendamento' });
+  }
+};
 
 
